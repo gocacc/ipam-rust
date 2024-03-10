463577e81886677a12a9f54bc896bdd9bf94bf7c
gocacc <125841970+gocacc@users.noreply.github.com>
2024-03-10 23:08:57 +0800

    Update routes.rs

--- routes.rs
+++ routes.rs
@@ -1,20 +1,50 @@
-use actix_web::{get, post, put, delete, web, HttpResponse};
-use serde::Deserialize;
+#[post("/ip_addresses")]
+async fn create_ip_address(
+    form: web::Form<IpAddressForm>,
+    db: web::Data<PgConnection>,
+) -> HttpResponse {
+    let new_ip_address = NewIpAddress {
+        ip_address: form.ip_address.clone(),
+        subnet_mask: form.subnet_mask.clone(),
+        gateway: form.gateway.clone(),
+        is_used: form.is_used,
+    };
 
-use crate::models::{IpAddress, NewIpAddress};
+    new_ip_address.insert(&db::connection()).unwrap();
 
-#[derive(Deserialize)]
-struct IpAddressForm {
-    ip_address: String,
-    subnet_mask: String,
-    gateway: String,
-    is_used: bool,
+    HttpResponse::SeeOther()
+        .header("Location", "/ip_addresses")
+        .finish()
 }
 
-#[get("/ip_addresses")]
-async fn ip_addresses(tera: web::Data<Tera>) -> HttpResponse {
-    let ip_addresses = IpAddress::all().load::<Vec<_>>(&db::connection()).unwrap();
-    let mut context = tera.context();
-    context.insert("ip_addresses", &ip_addresses);
-    let html = tera.render("ip_addresses.html", &context).unwrap();
-    HttpResponse::Ok().content_type("text/html").body(html
+#[put("/ip_addresses/{id}")]
+async fn update_ip_address(
+    id: web::Path<i32>,
+    form: web::Form<IpAddressForm>,
+    db: web::Data<PgConnection>,
+) -> HttpResponse {
+    let ip_address = IpAddress::find(id.into_inner()).load::<IpAddress>(&db::connection()).unwrap();
+
+    let updated_ip_address = IpAddress {
+        id: ip_address.id,
+        ip_address: form.ip_address.clone(),
+        subnet_mask: form.subnet_mask.clone(),
+        gateway: form.gateway.clone(),
+        is_used: form.is_used,
+    };
+
+    updated_ip_address.update(&db::connection()).unwrap();
+
+    HttpResponse::SeeOther()
+        .header("Location", "/ip_addresses")
+        .finish()
+}
+
+#[delete("/ip_addresses/{id}")]
+async fn delete_ip_address(id: web::Path<i32>, db: web::Data<PgConnection>) -> HttpResponse {
+    IpAddress::find(id.into_inner()).delete(&db::connection()).unwrap();
+
+    HttpResponse::SeeOther()
+        .header("Location", "/ip_addresses")
+        .finish()
+}
